cbd956989e5442d72fe9934349d84eb93fd3a36e
Youhar <haroun.beltaifa@gmail.com>
2017-08-10 15:01:20 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,89 @@
+/* global window,document */
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import MapGL from 'react-map-gl';
+import DeckGLOverlay from './deckgl-overlay.js';
+
+import {json as requestJson} from 'd3-request';
+
+// Set your mapbox token here
+const MAPBOX_TOKEN = "pk.eyJ1IjoieW91aGFyIiwiYSI6ImNqM294aDZneDAwZ3ozMW82bWxxNGk4eTMifQ.MAVwUeKB-xqbVTBFI3-MjA"; // eslint-disable-line
+
+// Source data GeoJSON
+const DATA_URL = 'https://energydata.info/dataset/a021ecbc-6ede-478f-8236-a8a2b093e363/resource/fe3ffd90-ffed-459a-9c52-bd91deb128f4/download/electric-network-mena.geojson'; // eslint-disable-line
+
+const colorScale = r => [r * 255, 255 * (1 - r), 255 * (r - 0.5) * (r - 0.5)];
+
+class Root extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                ...DeckGLOverlay.defaultViewport,
+                width: 1200,
+                height: 800
+            },
+            data: null
+        };
+
+        requestJson(DATA_URL, (error, response) => {
+            if (!error) {
+                this.setState({data: response});
+            }
+        });
+
+        this._onHover = this._onHover.bind(this)
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this._resize.bind(this));
+        this._resize();
+    }
+
+    _resize() {
+        this._onViewportChange({
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+    }
+
+    _onViewportChange(viewport) {
+        this.setState({
+            viewport: {...this.state.viewport, ...viewport}
+        });
+    }
+
+    _onHover(info) {
+        const hoverInfo = info;
+        if (hoverInfo !== this.state.hoverInfo) {
+            this.setState({hoverInfo});
+            console.log(hoverInfo.object);
+        }
+    }
+
+    render() {
+        const {viewport, data, hoverInfo} = this.state;
+
+        return (
+            <MapGL
+                {...viewport}
+                onViewportChange={this._onViewportChange.bind(this)}
+                mapboxApiAccessToken={MAPBOX_TOKEN}>
+                <DeckGLOverlay
+                    viewport={viewport}
+                    data={data}
+                    colorScale={colorScale}
+                    onHover = {this._onHover} />
+                {hoverInfo && <div className="tooltip" style={{left: hoverInfo.x, top: hoverInfo.y}}>
+                    <b> {hoverInfo.object.properties.name !== undefined && (hoverInfo.object.properties.name)} </b>
+                    <p> {hoverInfo.object.properties.nodeType !== undefined && (hoverInfo.object.properties.nodeType)} </p>
+                    <b> {hoverInfo.object.properties.transmissionPower !== undefined && (hoverInfo.object.properties.transmissionPower + 'kV ' + hoverInfo.object.properties.lineType + ' line')} </b>
+                    <p> {hoverInfo.object.properties.nodes !== undefined && ('Between ' + hoverInfo.object.properties.nodes.join(' & '))} </p>
+                </div>}
+            </MapGL>
+        );
+    }
+}
+
+render(<Root />, document.body.appendChild(document.createElement('div')));
---
+++ deckgl-overlay.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import {setParameters} from 'luma.gl';
+import DeckGL, {GeoJsonLayer} from 'deck.gl';
+
+const LIGHT_SETTINGS = {
+    lightsPosition: [0, 50.5, 8000, 30, 48.5, 8000],
+    ambientRatio: 0.2,
+    diffuseRatio: 0.5,
+    specularRatio: 0.3,
+    lightsStrength: [0.8, 0.0, 0.8, 0.0],
+    numberOfLights: 2
+};
+
+export default class DeckGLOverlay extends Component {
+
+    static get defaultViewport() {
+        return {
+            latitude: 30,
+            longitude: 22,
+            zoom: 3.2,
+            maxZoom: 16,
+            pitch: 30,
+            bearing: 0
+        };
+    }
+
+    _initialize(gl) {
+        setParameters(gl, {
+            depthTest: true,
+            depthFunc: gl.LEQUAL
+        });
+    }
+
+    render() {
+        const {viewport, data, colorScale} = this.props;
+
+        if (!data) {
+            return null;
+        }
+
+        const layer = new GeoJsonLayer({
+            id: 'geojson',
+            data,
+            opacity: 0.8,
+            stroked: false,
+            filled: true,
+            extruded: true,
+            wireframe: true,
+            fp64: true,
+            getRadius: f => {
+                if(f.properties.nodeType === "city") return 3000;
+                else return 1000
+            },
+            getFillColor: f => {
+                if(f.properties.nodeType === "city") return [255, 153, 51];
+                else if(f.properties.nodeType === "plant") return [255, 102, 102];
+                else if(f.properties.nodeType === "dam") return [51, 153, 255];
+                else if(f.properties.nodeType === "town") return [255, 153, 51];
+            },
+            getLineWidth: f => {
+                if(f.properties.lineType === "triple") return 1000;
+                else if(f.properties.lineType === "double")  return 800;
+                else if(f.properties.lineType === "single") return 400
+            },
+            getLineColor: f => colorScale(f.properties.transmissionPower/600),
+            lightSettings: LIGHT_SETTINGS,
+            pickable: Boolean(this.props.onHover),
+            onHover: this.props.onHover
+        });
+
+        return (
+            <DeckGL {...viewport} layers={ [layer] } onWebGLInitialized={this._initialize} />
+        );
+    }
+}
