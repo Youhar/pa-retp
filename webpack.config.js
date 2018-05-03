0591968c077c2f313b317cd9f7fefeed4919bcb4
Youhar <haroun.beltaifa@gmail.com>
2018-05-03 15:41:18 +0200

    updating dependencies and resolving build issues

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@
 const resolve = require('path').resolve;
 const webpack = require('webpack');
 
+// Otherwise modules imported from outside this directory does not compile
+// Seems to be a Babel bug
+// https://github.com/babel/babel-loader/issues/149#issuecomment-191991686
+const BABEL_CONFIG = {
+    presets: [
+      'es2015',
+      'stage-2',
+      'react'
+    ].map(name => require.resolve(`babel-preset-${name}`)),
+    plugins: [
+      'transform-decorators-legacy'
+    ].map(name => require.resolve(`babel-plugin-${name}`))
+  };
+
 const CONFIG = {
     entry: {
         app: resolve('./src/app.js')
@@ -14,12 +28,10 @@ const CONFIG = {
             {
                 // Compile ES2015 using buble
                 test: /\.js$/,
-                loader: 'buble-loader',
+                loader: 'babel-loader',
+                options: BABEL_CONFIG,
                 include: [resolve('.')],
                 exclude: [/node_modules/],
-                options: {
-                    objectAssign: 'Object.assign'
-                }
             }, {
                 test: /\.scss$/,
                 loaders: ['style-loader', 'css-loader', 'sass-loader', 'autoprefixer-loader']
@@ -28,11 +40,16 @@ const CONFIG = {
     },
 
     resolve: {
+        // Prefer root dependencies (dev) over local (prod)
+        modules: [resolve('../node_modules'), resolve('./node_modules')],
         alias: {
-            // From mapbox-gl-js README. Required for non-browserify bundlers (e.g. webpack):
-            'mapbox-gl$': resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js')
+          // Website is using React 15
+          react: resolve('.', './node_modules/react'),
+          'react-dom': resolve('.', './node_modules/react-dom'),
+          // From mapbox-gl-js README. Required for non-browserify bundlers (e.g. webpack):
+          'mapbox-gl$': resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js')
         }
-    },
+      },
 
     // Optional: Enables reading mapbox token from environment variable
     plugins: [
